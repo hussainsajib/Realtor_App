c32c4f1e8f70508a66b6790a40a899aa8979dbd0
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-03 19:39:31 -0400

    user login successful

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 
 from django.contrib.auth.models import User
 
@@ -44,8 +44,16 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login User
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Your are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
