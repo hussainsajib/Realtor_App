a4c59395cb9208be1ab108d3edec26d6bbfdcd5d
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-03 16:36:01 -0400

    added error message for register and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        # Register User
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        # Login User
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
