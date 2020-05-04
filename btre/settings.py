cf96705aac6991bb10f9131d2302ca13a0c27235
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-03 22:29:07 -0400

    added inquiry feature

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email config
+EMAIL_HOST = 'outlook.office365.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='mhisajib@myseneca.ca'
+EMAIL_HOST_PASSWORD='Function@6204'
+EMAIL_USE_TLS=True
